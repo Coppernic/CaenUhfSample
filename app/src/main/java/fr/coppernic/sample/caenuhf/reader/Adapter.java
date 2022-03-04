78473256707e30409bac53df14f545760d0e3b02
Florent Yvinec <florent.yvinec@coppernic.fr>
2022-03-04 16:40:21 +0100

    Updates sample code : gradle, plugin, dependancies, migrate to AndroidX

--- app/src/main/java/fr/coppernic/sample/caenuhf/reader/Adapter.java
+++ app/src/main/java/fr/coppernic/sample/caenuhf/reader/Adapter.java
@@ -1,7 +1,5 @@
 package fr.coppernic.sample.caenuhf.reader;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +7,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import fr.coppernic.sample.caenuhf.R;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
